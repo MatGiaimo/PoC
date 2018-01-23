d8f06500bb138c66743ed8a59b878fff5dc0653b
Mat Giaimo <mgaiimo@gmail.com>
2018-01-23 02:04:46 -0500

    Initial commit

---
+++ src/Page1.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar> {{title}} </custom-toolbar>
+    <p style="text-align: center">
+      <v-ons-search-input placeholder="Block Height, Hash, Address or Transaction id"></v-ons-search-input>
+      <v-ons-button @click="push"><v-ons-icon icon="fa-arrow-right"></v-ons-icon></v-ons-button>
+    </p>
+  </v-ons-page>
+</template>
+
+<script>
+  import customToolbar from './CustomToolbar';
+  import page2 from './Page2';
+  export default {
+     methods: {
+       pop(){
+         this.pageStack.pop();
+       },
+       push() {
+         this.pageStack.push(page2);
+       }
+     },
+     props: ['pageStack'],
+     components: { customToolbar },
+     data() {
+       return {
+         title: "NeoSpy - Neo Blockchain Explorer"
+       }
+     }
+  }
+</script>
---
+++ src/Page2.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar :back-label="Back">{{title}}</custom-toolbar>
+    <p style="text-align: center">Transaction Details</p>
+  </v-ons-page>
+</template>
+
+<script>
+import customToolbar from "./CustomToolbar";
+import app from "./App";
+
+export default {
+  methods: {
+    pop() {
+      this.pageStack.pop();
+    }
+  },
+  props: ["pageStack"],
+  components: { customToolbar },
+  data() {
+    return {
+      title: "Results"
+    };
+  }
+};
+</script>
