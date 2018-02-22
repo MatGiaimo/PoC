dfb24ff704ac6a0b4596e6a2fb6fd9b97b6e3f6b
Mat Giaimo <mgaiimo@gmail.com>
2018-02-22 18:30:42 -0500

    expanding details section

--- src/pages/TxDetails.vue
+++ src/pages/TxDetails.vue
@@ -2,38 +2,55 @@
   <v-ons-page>
     <custom-toolbar :back-label="back">{{title}}</custom-toolbar>
     <v-ons-card>
-      <div>
-        Type: {{tx.type}}
-      </div>
+      <v-ons-row>
+        <v-ons-col>Type:</v-ons-col>
+        <v-ons-col>{{tx.type}}</v-ons-col>
+      </v-ons-row>
+            <v-ons-row>
+        <v-ons-col>Transaction ID:</v-ons-col>
+        <v-ons-col>{{tx.txid}}</v-ons-col>
+      </v-ons-row>
+            <v-ons-row>
+        <v-ons-col>Time:</v-ons-col>
+        <v-ons-col>{{moment(tx.time*1000).format()}}</v-ons-col>
+      </v-ons-row>
     </v-ons-card>
   </v-ons-page>
 </template>
 
 <script>
-import customToolbar from "../partials/CustomToolbar.vue";
-import app from "../App.vue";
-import apiMaker from '../api/ApiMaker';
+  import customToolbar from "../partials/CustomToolbar.vue";
+  import app from "../App.vue";
+  import apiMaker from '../api/ApiMaker';
 
-var explorer = apiMaker.Explorer('neoscanio');
+  let moment = require('moment');
 
-export default {
-  methods: {
-    pop() {
-      this.pageStack.pop();
+  var explorer = apiMaker.Explorer('neoscanio');
+
+  export default {
+    methods: {
+      pop() {
+        this.pageStack.pop();
+      }
+    },
+    mounted() {
+      explorer.getTransaction(this.txid).then((data) => {
+        this.tx = data
+        console.dir(this.tx);
+      });
+    },
+    props: ["pageStack", "hash"],
+    components: {
+      customToolbar
+    },
+    data() {
+      return {
+        title: "Transaction Information",
+        tx: {},
+        txid: "",
+        back: "Back",
+        moment: moment
+      };
     }
-  },
-  mounted() {
-      explorer.getTransaction(this.txid).then((data) => this.tx = data);
-  },
-  props: ["pageStack","hash"],
-  components: { customToolbar },
-  data() {
-    return {
-      title: "Transaction Information",
-      tx: {},
-      txid: "",
-      back: "Back"
-    };
-  }
-};
+  };
 </script>
--- src/partials/TransactionList.vue
+++ src/partials/TransactionList.vue
@@ -11,7 +11,7 @@
       </v-ons-list-item>
       <v-ons-list-item :key="n" v-for="n in 5" tappable @click="details(transactions[n].txid)">
           <div class="left">{{transactions[n].type}}</div>
-          <div class="center">{{String(transactions[n].txid).slice(0,12)+'...'}}</div>
+          <div class="center">{{String(transactions[n].txid).slice(0,12) + '...'}}</div>
           <div class="right">{{moment(transactions[n].time*1000).format()}}</div>
       </v-ons-list-item>
     </v-ons-list>
@@ -26,6 +26,22 @@
   export default {
     props: ["transactions", "pageStack"],
     methods: {
+      getTxType(tx) {
+        return tx.type;
+      },
+      getTxId(tx) {
+        return String(tx.txid).slice(0,12) + '...';
+      },
+      getTimeStamp(tx) {
+        return moment(tx.time*1000).format();
+      },
+      getTxVm(tx) {
+        return {
+          type: getTxType(tx),
+          id: getTxId(tx),
+          time: getTimeStamp(tx)
+        }
+      },
       details(txid) {
          this.pageStack.push({
            extends: txDetails,
